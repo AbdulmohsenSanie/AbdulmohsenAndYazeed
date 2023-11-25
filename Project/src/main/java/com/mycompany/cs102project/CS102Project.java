bb6cfc94461a42dc470bd53689b71b928b54c2ba
AbdulmohsenSanie <71731943+AbdulmohsenSanie@users.noreply.github.com>
2023-11-25 15:21:17 +0300

    Add files via upload

---
+++ Project/src/main/java/com/mycompany/cs102project/CS102Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cs102project;
+
+/**
+ *
+ * @author yazee
+ */
+public class CS102Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
